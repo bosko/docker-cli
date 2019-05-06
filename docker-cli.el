49793e45652f4c16c23a6b3421a5bc7ea2690a2d
Boško Ivanišević <bosko.ivanisevic@gmail.com>
2019-05-06 20:56:10 +0200

    feat: composing arguments in a function
    Instead of listing arguments each command definition now has compose
    function option. Function should gather all arguments for the Docker
    container command and return it as a list of strings.

--- docker-cli.el
+++ docker-cli.el
@@ -12,13 +12,30 @@
 (defvar docker-cli-commands-alist
   '((psql
      :command "psql"
-     :arguments ("-U" "postgres" "-P" "pager=off"))
+     :arguments-compose-func docker-cli-psql-arguments)
 
     (redis-cli
      :command "redis-cli"
-     :arguments nil)
+     :arguments-compose-func nil)
     )
-  "An alist of defined commands that can be ran in docker container.")
+  "An alist of defined commands that can be ran in docker container.
+
+Each element in the list must be of the following format:
+
+  (COMMAND-KEY FEATURE VALUE)
+
+where COMMAND-KEY is unique value that determins command and can
+be displayed in selection when `docker-run' is executed. Each key
+is followed by FEATURE-VALUE pairs. Feature can be any of following:
+
+  :command                    Command that will be executed in the
+                              Docker container.
+
+  :arguments-compose-func     Function without arguments that will be
+                              called in order to fetch all command
+                              arguments.
+
+New commands can be supported by adding new element to this list.")
 
 (defvar docker-cli-mode-map
   (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
@@ -26,6 +43,21 @@
     (define-key map "\t" 'completion-at-point)
     map)
   "Basic mode map for `run-docker'")
+
+(defcustom docker-cli-db-username ""
+  "Database username."
+  :type 'string
+  :group 'DockerCLI)
+
+(defcustom docker-cli-db-name ""
+  "Database name"
+  :type 'string
+  :group 'DockerCLI)
+
+(defcustom docker-cli-host ""
+  "Host name"
+  :type 'string
+  :group 'DockerCLI)
 
 ;; This value is for psql. It should be nil here and
 ;; set depending of command started
@@ -37,16 +69,28 @@
 (defvar docker-cli-prompt-cont-regexp "^[[:alnum:]_]*=[#>] "
   "Prompt pattern for continuation prompt.")
 
-(defun docker-cli-select-command ()
-  (let ((commands (mapcar 'symbol-name (mapcar 'car docker-cli-commands-alist))))
-    (ido-completing-read "Command: " commands)))
+(defun docker-cli-psql-arguments ()
+  "Composes arguments for running PSQL in docker container"
+  (setq docker-cli-db-username (read-string "Username: " docker-cli-db-username))
+  (setq docker-cli-db-name (read-string "Database: " docker-cli-db-name))
+  (setq docker-cli-host (read-string "Host: " docker-cli-host))
+  `("-U" ,docker-cli-db-username "-h" ,docker-cli-host "-P" "pager=off" ,docker-cli-db-name)
+  )
+
+(defun docker-cli-select-option (prompt options)
+  (ido-completing-read prompt options))
 
 (defun docker-cli-compose-params ()
-  (let* ((curr-command-name (docker-cli-select-command))
+  (let* ((container (docker-cli-select-option
+                     "Container: "
+                     (split-string (shell-command-to-string "docker ps --format '{{.Names}}'"))))
+         (curr-command-name (docker-cli-select-option
+                             "Command: "
+                             (mapcar 'symbol-name (mapcar 'car docker-cli-commands-alist))))
          (curr-command (cdr (assoc (intern curr-command-name) docker-cli-commands-alist)))
-         (params (plist-get curr-command :arguments)))
+         (params (apply (plist-get curr-command :arguments-compose-func) nil)))
     (setq params (cons (plist-get curr-command :command) params))
-    (setq params (cons "pg11" params))
+    (setq params (cons container params))
     (setq params (append docker-cli-exec-arguments params))))
 
 (defun run-docker ()
